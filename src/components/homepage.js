3b276f7e168f85eb3c0d7aa698e800396d2d5561
samdhein <hein.sam@gmail.com>
2022-01-11 23:10:40 -0800

    completed assignment

---
+++ src/components/homepage.js
@@ -0,0 +1,18 @@
+import React from "react";
+class Homepage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Dojo</h1>
+                <h3>Things I need to do:</h3>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Climb Mt. Everest</li>
+                    <li>Run a marathon</li>
+                    <li>Feed the dogs</li>
+                </ul>
+            </div>
+        );
+    }
+}
+export default Homepage;
